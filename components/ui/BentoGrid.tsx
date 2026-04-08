de4d08657cfa451c69e30ac4f831317516943197
Andrew <3922017@holycross.edu.ph>
2026-04-09 07:25:52 +0800

    update

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -25,7 +25,7 @@ export const BentoGrid = ({
       className={cn(
         // change gap-4 to gap-8, change grid-cols-3 to grid-cols-5, remove md:auto-rows-[18rem], add responsive code
         "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
-        className
+        className,
       )}
     >
       {children}
@@ -53,7 +53,7 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["Bootstrap", "React.JS", "Typescript"];
+  const leftLists = ["Tailwind", "React.JS", "JavaScript"];
   const rightLists = ["PHP", "Next.JS", "Mysql"];
 
   const [copied, setCopied] = useState(false);
@@ -89,7 +89,7 @@ export const BentoGridItem = ({
       className={cn(
         // remove p-4 rounded-3xl dark:bg-black dark:border-white/[0.2] bg-white  border border-transparent, add border border-white/[0.1] overflow-hidden relative
         "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
-        className
+        className,
       )}
       style={{
         //   add these two
@@ -134,7 +134,7 @@ export const BentoGridItem = ({
         <div
           className={cn(
             titleClassName,
-            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10",
           )}
         >
           {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
